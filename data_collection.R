2a22f5ba3d597b13318660fdc969570305a11362
lindseygreenhill <lgreenhill@college.harvard.edu>
2021-10-25 15:53:09 -0400

    new data

---
+++ data_collection.R
@@ -0,0 +1,17 @@
+# The third eye data starts at August 25th 2017. Today is October 25, 2021. I am
+# going to take a random sample of 10 weeks from the 217 weeks in between the
+# dates because you can only download the data 1 week at a time. It is obviously
+# not as comprehensive as doing all of the weeks. 
+
+set.seed(1121)
+
+random_numbers <- sample(1:217, 10, replace = FALSE)
+
+# the numbers generated above are 193, 205, 96, 175, 150, 22, 109, 44, 83, 92.
+# This translates into start dates of 05/7/2021, 07/30/2021, 06/28/2019,
+# 01/01/2021, 07/10/2020, 01/26/2018, 09/27/2019, 06/29/2018, 03/29/2019,
+# 05/31/2019. One of the things I noticed is that there seems to be a lot of
+# summer months in here. If I have time I should do a sensitivity analysis using
+# different samples
+
+data_1
