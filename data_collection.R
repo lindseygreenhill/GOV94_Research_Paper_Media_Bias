80f5fd6c162cd2fde8c83f62e3f39a3070bacf01
lindseygreenhill <lgreenhill@college.harvard.edu>
2021-10-25 17:56:43 -0400

    data changed

--- data_collection.R
+++ data_collection.R
@@ -14,4 +14,33 @@ random_numbers <- sample(1:217, 10, replace = FALSE)
 # summer months in here. If I have time I should do a sensitivity analysis using
 # different samples
 
-data_1
+data_1 <- read_csv("data/third_eye_01_01_21.csv")
+data_2 <- read_csv("data/third_eye_01_26_18.csv")
+data_3 <- read_csv("data/third_eye_03_29_19.csv")
+data_4 <- read_csv("data/third_eye_05_31_19.csv")
+data_5 <- read_csv("data/third_eye_06_28_19.csv")
+data_6 <- read_csv("data/third_eye_07_10_20.csv")
+data_7 <- read_csv("data/third_eye_06_29_18.csv")
+data_8 <- read_csv("data/third_eye_09_27_19.csv")
+
+# joining all of the data frames
+
+data_joined <- data_1 %>%
+  bind_rows(data_2, 
+            data_3,
+            data_4,
+            data_5,
+            data_6,
+            data_7,
+            data_8)
+
+summary(data_joined)
+
+tidy_data <- data_joined %>%
+  clean_names() %>%
+  filter(channel != "BBCNEWS") %>%
+  mutate(date_sub = substr(date_time_utc, 1, 10),
+         date = ymd(date_sub)) %>%
+  select(-date_sub, - date_time_utc, -https_archive_org_details)
+
+write_csv(tidy_data, "tidy_chyron_data.csv")
